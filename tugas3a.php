4d71990ac5edf9432e485383652f78586021beec
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-10-07 12:12:12 +0700

    Add files via upload

---
+++ tugas3a.php
@@ -0,0 +1,11 @@
+<?php
+	$S = 360;
+	$V = 90;
+	$T = $S/$V;
+
+
+	echo "Jarak Kota A-Z = $S <br>";
+	echo "Kecepatan Motor = $V <br>";
+	echo "Waktu tempuh = ? <br>";
+	echo "Jawaban : $T Menit";
+?>
---
+++ tugas3b.php
@@ -0,0 +1,11 @@
+<?php
+	$awal = 150000;
+	$bunga = 0.125;
+	$tabungan=($awal*$bunga)+$awal;
+
+
+	echo "Tabungan Awal = Rp $awal <br>";
+	echo "Bunga Setahun = 12.5% <br>";
+	echo "Total Tabungan Setahun = ? <br>";
+	echo "Jawaban : Rp $tabungan";
+?>
